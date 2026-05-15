227fc5de377eff01ec5b27de69c2bb48bb2268d3
hilaldemir01 <hilal.demir00@outlook.com>
2026-05-16 02:54:06 +0300

    fetch data from the backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,7 +1,15 @@
 import "./HomePage.css";
 import { products } from "../../starting-code/data/products";
 import { Header } from "../components/Header";
+
 export function HomePage() {
+  fetch("http://localhost:3000/api/products")
+  .then((response) => {
+     response.json().then((data) => {
+       return response.json().then((data) => {
+         console.log(data);
+     });
+  })
   return (
     <>
       <title>Ecommerce Project</title>
@@ -12,10 +20,7 @@ export function HomePage() {
             return (
               <div key={product.id} className="product-container">
                 <div className="product-image-container">
-                  <img
-                    className="product-image"
-                    src={product.image}
-                  />
+                  <img className="product-image" src={product.image} />
                 </div>
 
                 <div className="product-name limit-text-to-2-lines">
@@ -25,12 +30,18 @@ export function HomePage() {
                 <div className="product-rating-container">
                   <img
                     className="product-rating-stars"
-                    src={"images/ratings/rating-{product.rating.start * 10}.png"}
+                    src={
+                      "images/ratings/rating-{product.rating.start * 10}.png"
+                    }
                   />
-                  <div className="product-rating-count link-primary">{product.rating.count}</div>
+                  <div className="product-rating-count link-primary">
+                    {product.rating.count}
+                  </div>
                 </div>
 
-                <div className="product-price">{(product.priceCents / 100).toFixed(2)}</div>
+                <div className="product-price">
+                  {(product.priceCents / 100).toFixed(2)}
+                </div>
 
                 <div className="product-quantity-container">
                   <select>

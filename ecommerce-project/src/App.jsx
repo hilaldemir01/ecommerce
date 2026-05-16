3bdd8106becf2a16d10c3f5cda5f0038d3cb465f
hilaldemir01 <hilal.demir00@outlook.com>
2026-05-16 14:51:07 +0300

    Generate HTML for the cart items

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -9,15 +9,15 @@ import './App.css'
 
 function App() {
   const [cart, setCart] = useState([]);
-  const [products, setProducts] = useState([]);
+ // const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    axios.get("/api/products")
+  /*  axios.get("/api/products")
     .then((response) => {
       setProducts(response.data);
-    });
+    });*/
 
-    axios.get("/api/cart-items")
+    axios.get("/api/cart-items?expand=product")
     .then((response) => {
       setCart(response.data);
     });
